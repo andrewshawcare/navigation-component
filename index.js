b473bc689c0cbf8c19f204986c584b151a9ff13b
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-05-30 22:44:51 -0400

    Renders an ordered collection of links

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 define([], function () {
-  return function () {
+  return function (args) {
+    var args = args || {};
+    var links = args.links || [];
+
     var navigationElement = document.createElement("navigation");
     navigationElement.classList.add("navigation");
+
+    var linkCollectionElement = document.createElement("ol");
+    linkCollectionElement.classList.add("linkCollection");
+    links.forEach(function (link) {
+      var linkElement = document.createElement("li");
+      linkElement.classList.add("link");
+
+      var titleElement = document.createElement("a");
+      titleElement.classList.add("title");
+      titleElement.textContent = link.title;
+      titleElement.attributes["href"] = link.url;
+      linkElement.appendChild(titleElement);
+
+      linkCollectionElement.appendChild(linkElement);
+    });
+    navigationElement.appendChild(linkCollectionElement);
+
     return navigationElement;
   };
 });
--- spec.js
+++ spec.js
@@ -8,5 +8,27 @@ define(["./index.js"], function (NavigationComponent) {
     it("has the navigation class", function () {
       expect(NavigationComponent().classList.contains("navigation")).toBe(true);
     });
+
+    it("Renders an ordered collection of links", function () {
+      var links = [
+        {title: "Foo", url: "http://www.example.com/foo"},
+        {title: "Bar", url: "http://www.example.com/bar"},
+        {title: "Baz", url: "http://www.example.com/baz"}
+      ];
+      var navigationElement = NavigationComponent({
+        links: links
+      });
+
+      links.forEach(function (link, index) {
+        var titleElement = navigationElement.querySelector(
+          ".linkCollection .link:nth-child(0n+" +
+          (index + 1) +
+          ") .title"
+        );
+
+        expect(titleElement.textContent).toBe(link.title);
+        expect(titleElement.attributes["href"]).toBe(link.url);
+      });
+    });
   });
 });
