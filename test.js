8cdf652bde3fabb13b836a109ab4534e26cd3d3b
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-06-04 10:41:09 -0500

    Decouple test structure

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 var JsDom = require("jsdom");
-var Jasmine = require("jasmine");
-var RequireJs = require("requirejs");
-
-global.define = RequireJs.define;
 global.window = JsDom.jsdom().defaultView;
 global.document = window.document;
 
+var RequireJs = require("requirejs");
+global.define = RequireJs.define;
 RequireJs.config({
   paths: {"node_modules": "./node_modules"},
   nodeRequire: require
 });
 
+var Jasmine = require("jasmine");
 var jasmine = new Jasmine();
 RequireJs(["./spec.js"], function () {
   jasmine.execute();
